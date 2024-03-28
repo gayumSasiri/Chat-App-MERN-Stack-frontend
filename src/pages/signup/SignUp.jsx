110ad241805feb0f5444d586cafc66840b481fa3
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2024-03-28 12:48:44 +0530

    Sign Up UI finished

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,6 +1,130 @@
+import GenderCheckbox from "./GenderCheckbox";
+
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          SignUp
+          <span className="text-blue-500"> CyberConnect</span>
+        </h1>
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input type="text" placeholder="Eg: John Doe Cristoper" className="w-full input input-bordered h-10" />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Userame</span>
+            </label>
+            <input type="text" placeholder="Eg: johnDoe" className="w-full input input-bordered h-10" />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input 
+                type="password"
+                placeholder="Enter your Password"
+                className="w-full input input-bordered h-10"
+              />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Conform Password</span>
+            </label>
+            <input 
+                type="password"
+                placeholder="Enter your Password again"
+                className="w-full input input-bordered h-10"
+              />
+          </div>
+
+          <GenderCheckbox />
+
+          <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+            Already have an account?
+          </a>
+
+          <div>
+            <button className="btn btn-block btn-sm mt-2">Sign Up</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
   )
 }
 export default SignUp;
+
+
+//  STARTER CODE FOR THE SIGN UP COMPONENT
+
+// import GenderCheckbox from "./GenderCheckbox";
+
+// const SignUp = () => {
+//   return (
+//     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+//       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+//         <h1 className="text-3xl font-semibold text-center text-gray-300">
+//           SignUp
+//           <span className="text-blue-500"> CyberConnect</span>
+//         </h1>
+//         <form>
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Full Name</span>
+//             </label>
+//             <input type="text" placeholder="Eg: John Doe Cristoper" className="w-full input input-bordered h-10" />
+//           </div>
+
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Userame</span>
+//             </label>
+//             <input type="text" placeholder="Eg: johnDoe" className="w-full input input-bordered h-10" />
+//           </div>
+
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Password</span>
+//             </label>
+//             <input 
+//                 type="password"
+//                 placeholder="Enter your Password"
+//                 className="w-full input input-bordered h-10"
+//               />
+//           </div>
+
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Conform Password</span>
+//             </label>
+//             <input 
+//                 type="password"
+//                 placeholder="Enter your Password again"
+//                 className="w-full input input-bordered h-10"
+//               />
+//           </div>
+
+//           <GenderCheckbox />
+
+//           <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+//             Already have an account?
+//           </a>
+
+//           <div>
+//             <button className="btn btn-block btn-sm mt-2">Sign Up</button>
+//           </div>
+
+//         </form>
+//       </div>
+//     </div>
+//   )
+// }
+// export default SignUp;
